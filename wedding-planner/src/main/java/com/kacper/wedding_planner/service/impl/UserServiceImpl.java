7f04bcd10544fa64370cb2fb0addf28ba2c68bfa
Kacper Szabat <kacper.szabat99@gmail.com>
2025-06-17 17:48:28 +0200

    The welcome email is sent correctly

--- wedding-planner/src/main/java/com/kacper/wedding_planner/service/impl/UserServiceImpl.java
+++ wedding-planner/src/main/java/com/kacper/wedding_planner/service/impl/UserServiceImpl.java
@@ -2,6 +2,7 @@ package com.kacper.wedding_planner.service.impl;
 
 import com.kacper.wedding_planner.model.User;
 import com.kacper.wedding_planner.repository.UserRepository;
+import com.kacper.wedding_planner.service.EmailService;
 import com.kacper.wedding_planner.service.UserService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -12,10 +13,12 @@ public class UserServiceImpl implements UserService {
 
     private final UserRepository userRepository;
     private final PasswordEncoder passwordEncoder;
+    private final EmailService emailService;
 
-    public UserServiceImpl(UserRepository userRepository, PasswordEncoder passwordEncoder) {
+    public UserServiceImpl(UserRepository userRepository, PasswordEncoder passwordEncoder, EmailService emailService) {
         this.userRepository = userRepository;
         this.passwordEncoder = passwordEncoder;
+        this.emailService = emailService;
     }
 
     @Override
@@ -30,6 +33,8 @@ public class UserServiceImpl implements UserService {
         user.setFirstName(firstName);
 
         userRepository.save(user);
+
+        emailService.sendWelcomeEmail(email, firstName);
     }
 
     @Override
