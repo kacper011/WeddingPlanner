951c62091092eb1653cb6515baf77f9d022ea509
Kacper Szabat <kacper.szabat99@gmail.com>
2025-06-22 21:09:25 +0200

    Writing methods in EventServiceImpl

--- wedding-planner/src/main/java/com/kacper/wedding_planner/service/impl/EventServiceImpl.java
+++ wedding-planner/src/main/java/com/kacper/wedding_planner/service/impl/EventServiceImpl.java
@@ -1,6 +1,9 @@
 package com.kacper.wedding_planner.service.impl;
 
 import com.kacper.wedding_planner.model.Event;
+import com.kacper.wedding_planner.model.User;
+import com.kacper.wedding_planner.repository.EventRepository;
+import com.kacper.wedding_planner.repository.UserRepository;
 import com.kacper.wedding_planner.service.EventService;
 import org.springframework.stereotype.Service;
 
@@ -8,13 +11,24 @@ import java.util.List;
 
 @Service
 public class EventServiceImpl implements EventService {
+
+    private final EventRepository eventRepository;
+    private final UserRepository userRepository;
+
+    public EventServiceImpl(EventRepository eventRepository, UserRepository userRepository) {
+        this.eventRepository = eventRepository;
+        this.userRepository = userRepository;
+    }
+
     @Override
     public List<Event> getEventsForUser(String email) {
-        return null;
+        return eventRepository.findByUserEmail(email);
     }
 
     @Override
     public void saveEventForUser(Event event, String email) {
-
+        User user = userRepository.findByEmail(email).orElseThrow();
+        event.setUser(user);
+        eventRepository.save(event);
     }
 }
