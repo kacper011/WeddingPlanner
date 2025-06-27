165c7e38393e89dd7db0fa1c86de23c434537e99
Kacper Szabat <kacper.szabat99@gmail.com>
2025-06-27 20:13:40 +0200

    Writing a test shouldSendReminderEmailSuccessfully

--- wedding-planner/src/test/java/com/kacper/wedding_planner/service/EmailServiceTest.java
+++ wedding-planner/src/test/java/com/kacper/wedding_planner/service/EmailServiceTest.java
@@ -5,6 +5,8 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.mail.javamail.JavaMailSender;
 
+import java.time.LocalDate;
+
 import static org.mockito.Mockito.*;
 
 public class EmailServiceTest {
@@ -27,4 +29,15 @@ public class EmailServiceTest {
 
         verify(mailSender, times(1)).send(any(MimeMessage.class));
     }
+
+    @Test
+    void shouldSendReminderEmailSuccessfully() throws Exception{
+        MimeMessage mimeMessage = mock(MimeMessage.class);
+        when(mailSender.createMimeMessage()).thenReturn(mimeMessage);
+
+        emailService.sendReminderEmail("user@example.com", "Kacper", "Wesele", LocalDate.of(2025, 6, 29));//Trzeba ustawiać datę na 2 dni przed
+
+        verify(mailSender, times(1)).send(any(MimeMessage.class));
+    }
 }
+
