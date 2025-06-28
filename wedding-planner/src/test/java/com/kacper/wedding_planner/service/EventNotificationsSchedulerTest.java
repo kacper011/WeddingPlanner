8c8fdca005f9d7b8847f9f1faf38bce994a2d4c0
Kacper Szabat <kacper.szabat99@gmail.com>
2025-06-28 18:21:45 +0200

    Writing a test shouldSendReminderAndMarkEventAsSent

--- wedding-planner/src/test/java/com/kacper/wedding_planner/service/EventNotificationsSchedulerTest.java
+++ wedding-planner/src/test/java/com/kacper/wedding_planner/service/EventNotificationsSchedulerTest.java
@@ -1,12 +1,17 @@
 package com.kacper.wedding_planner.service;
 
+import com.kacper.wedding_planner.model.Event;
+import com.kacper.wedding_planner.model.User;
 import com.kacper.wedding_planner.repository.EventRepository;
 import com.kacper.wedding_planner.repository.UserRepository;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.time.LocalDate;
 import java.util.Collections;
+import java.util.List;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.*;
 
@@ -33,5 +38,26 @@ public class EventNotificationsSchedulerTest {
 
         verify(emailService, never()).sendReminderEmail(any(), any(), any(), any());
         verify(eventRepository, never()).save(any());
+    }
+
+    @Test
+    void shouldSendReminderAndMarkEventAsSent() {
+        User user = new User();
+        user.setEmail("test@example.com");
+        user.setFirstName("Kacper");
+
+        Event event = new Event();
+        event.setUser(user);
+        event.setTitle("Wesele");
+        event.setDate(LocalDate.now().plusDays(2));
+        event.setReminderSent(false);
+
+        when(eventRepository.findByDateAndReminderSentFalse(any())).thenReturn(List.of(event));
+
+        scheduler.sendReminders();
+
+        verify(emailService).sendReminderEmail("test@example.com", "Kacper", "Wesele", event.getDate());
+        verify(eventRepository).save(event);
+        assertTrue(event.isReminderSent());
     }
 }
