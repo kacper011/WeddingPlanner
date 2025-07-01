176073334b263482e34c468315e09847941dd840
Kacper Szabat <kacper.szabat99@gmail.com>
2025-07-01 20:50:04 +0200

    Events are displayed correctly in the calendar

--- wedding-planner/src/main/java/com/kacper/wedding_planner/controller/EventController.java
+++ wedding-planner/src/main/java/com/kacper/wedding_planner/controller/EventController.java
@@ -1,6 +1,8 @@
 package com.kacper.wedding_planner.controller;
 
 import com.kacper.wedding_planner.config.CustomUserDetails;
+import com.kacper.wedding_planner.dto.EventDTO;
+import com.kacper.wedding_planner.mapper.EventMapper;
 import com.kacper.wedding_planner.model.Event;
 import com.kacper.wedding_planner.model.User;
 import com.kacper.wedding_planner.repository.EventRepository;
@@ -31,30 +33,34 @@ public class EventController {
 
     @GetMapping("/data")
     @ResponseBody
-    public List<Event> getEventsJson(@AuthenticationPrincipal CustomUserDetails principal) {
-        return eventRepository.findByUserEmail(principal.getUsername());
+    public List<EventDTO> getEventsJson(@AuthenticationPrincipal CustomUserDetails principal) {
+        List<Event> events = eventRepository.findByUserEmail(principal.getUsername());
+        return events.stream()
+                .map(EventMapper::toDTO)
+                .toList();
     }
 
     @PostMapping
     @ResponseBody
-    public Event createEvent(@RequestBody Event event, @AuthenticationPrincipal CustomUserDetails principal) {
+    public EventDTO createEvent(@RequestBody Event event, @AuthenticationPrincipal CustomUserDetails principal) {
         User user = userRepository.findByEmail(principal.getUsername()).orElseThrow(() -> new RuntimeException("User not found"));
 
         event.setUser(user);
-        return eventRepository.save(event);
+        Event saved = eventRepository.save(event);
+        return EventMapper.toDTO(saved);
     }
 
     @PutMapping("/{id}")
     @ResponseBody
-    public Event updateEvent(@PathVariable Long id, @RequestBody Event updated, @AuthenticationPrincipal CustomUserDetails principal) {
+    public EventDTO updateEvent(@PathVariable Long id, @RequestBody Event updated, @AuthenticationPrincipal CustomUserDetails principal) {
         return eventRepository.findById(id)
                 .filter(event -> event.getUser().getEmail().equals(principal.getUsername()))
                 .map(event -> {
                     event.setTitle(updated.getTitle());
                     event.setDate(updated.getDate());
-                    return eventRepository.save(event);
-                })
-                .orElseThrow(() -> new RuntimeException("Event not found or unauthorized"));
+                    Event saved = eventRepository.save(event);
+                    return EventMapper.toDTO(saved);
+                }).orElseThrow(() -> new RuntimeException("Event not found or unauthorized"));
     }
 
     @DeleteMapping("/{id}")
