450cfb7ee9e6a10014957fa1ecf91fa9661b0992
Kacper Szabat <kacper.szabat99@gmail.com>
2025-07-01 20:32:24 +0200

    Addition of EventMapper to app

--- wedding-planner/src/main/java/com/kacper/wedding_planner/mapper/EventMapper.java
+++ wedding-planner/src/main/java/com/kacper/wedding_planner/mapper/EventMapper.java
@@ -1,4 +1,14 @@
 package com.kacper.wedding_planner.mapper;
 
+import com.kacper.wedding_planner.dto.EventDTO;
+import com.kacper.wedding_planner.model.Event;
+
 public class EventMapper {
+    public static EventDTO toDTO(Event event) {
+        EventDTO dto = new EventDTO();
+        dto.setId(event.getId());
+        dto.setTitle(event.getTitle());
+        dto.setDate(event.getDate());
+        return dto;
+    }
 }
