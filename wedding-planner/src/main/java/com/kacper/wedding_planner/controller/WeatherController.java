326d49b67b357113081b8e202ec9f6d0e6e0b725
Kacper Szabat <kacper.szabat99@gmail.com>
2025-07-08 21:47:12 +0200

    Addition of weather to app

--- wedding-planner/src/main/java/com/kacper/wedding_planner/controller/WeatherController.java
+++ wedding-planner/src/main/java/com/kacper/wedding_planner/controller/WeatherController.java
@@ -1,20 +1,31 @@
 package com.kacper.wedding_planner.controller;
 
 import com.kacper.wedding_planner.config.CustomUserDetails;
+import com.kacper.wedding_planner.dto.WeatherResponse;
+import com.kacper.wedding_planner.model.User;
+import com.kacper.wedding_planner.service.UserService;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.client.RestTemplate;
 
 @Controller
-@RequestMapping("/weather")
 public class WeatherController {
 
-    @GetMapping
-    public String showWeather(@AuthenticationPrincipal CustomUserDetails principal, Model model) {
+    private final UserService userService;
 
+    public WeatherController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping("/guests/countdown/weather")
+    @ResponseBody
+    public WeatherResponse getWeatherData(@AuthenticationPrincipal CustomUserDetails principal, Model model) {
+
+        User user = userService.findByEmail(principal.getUsername());
         double latitude = 51.4255;
         double longitude = 23.0617;
 
@@ -22,9 +33,11 @@ public class WeatherController {
                 "?latitude=" + latitude +
                 "&longitude=" + longitude +
                 "&daily=temperature_2m_max,temperature_2m_min,precipitation_sum" +
-                "&timezone=Europe%2FWarsaw";
+                "&forecast_days=10" +
+                "&timezone=auto";
 
         RestTemplate restTemplate = new RestTemplate();
-        WeatherResponse response
+        return restTemplate.getForObject(url, WeatherResponse.class);
+
     }
 }
