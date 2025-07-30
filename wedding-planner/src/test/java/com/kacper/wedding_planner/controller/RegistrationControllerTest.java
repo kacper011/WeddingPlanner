f539b4d8d4b0158e417b23e9c777b4fc9ffb1a13
Kacper Szabat <kacper.szabat99@gmail.com>
2025-07-30 21:39:32 +0200

    Writing a test shouldShowRegistrationFrom

---
+++ wedding-planner/src/test/java/com/kacper/wedding_planner/controller/RegistrationControllerTest.java
@@ -0,0 +1,36 @@
+package com.kacper.wedding_planner.controller;
+
+import com.kacper.wedding_planner.config.SecurityConfig;
+import com.kacper.wedding_planner.service.UserService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.Import;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@WebMvcTest(RegistrationController.class)
+@Import(SecurityConfig.class)
+class RegistrationControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private UserService userService;
+
+    @MockBean
+    private UserDetailsService userDetailsService;
+
+    @Test
+    void shouldShowRegistrationForm() throws Exception {
+        mockMvc.perform(get("/register"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("register"))
+                .andExpect(model().attributeExists("userDto"));
+    }
+}
