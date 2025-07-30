378c0e78dc0961222c5dddfca9a8d59ce44aaf05
Kacper Szabat <kacper.szabat99@gmail.com>
2025-07-30 21:41:35 +0200

    Writing a test shouldRegisterUserSuccessfully

--- wedding-planner/src/test/java/com/kacper/wedding_planner/controller/RegistrationControllerTest.java
+++ wedding-planner/src/test/java/com/kacper/wedding_planner/controller/RegistrationControllerTest.java
@@ -3,6 +3,7 @@ package com.kacper.wedding_planner.controller;
 import com.kacper.wedding_planner.config.SecurityConfig;
 import com.kacper.wedding_planner.service.UserService;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
@@ -11,6 +12,7 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @WebMvcTest(RegistrationController.class)
@@ -32,5 +34,17 @@ class RegistrationControllerTest {
                 .andExpect(status().isOk())
                 .andExpect(view().name("register"))
                 .andExpect(model().attributeExists("userDto"));
+    }
+
+    @Test
+    void shouldRegisterUserSuccessfully() throws Exception {
+        mockMvc.perform(post("/register")
+                        .param("email", "test@example.com")
+                        .param("password", "secret123")
+                        .param("firstName", "John"))
+                .andExpect(status().is3xxRedirection())
+                .andExpect(redirectedUrl("/login?success"));
+
+        Mockito.verify(userService).registerUser("test@example.com", "secret123", "John");
     }
 }
