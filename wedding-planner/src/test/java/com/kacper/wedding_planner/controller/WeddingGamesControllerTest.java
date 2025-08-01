03d5b816458a01aedd06779a2c0c1170df9918b1
Kacper Szabat <kacper.szabat99@gmail.com>
2025-08-01 19:59:18 +0200

    Writing a test shouldWeddingGamesPageReturnsViewWithQuestions

---
+++ wedding-planner/src/test/java/com/kacper/wedding_planner/controller/WeddingGamesControllerTest.java
@@ -0,0 +1,62 @@
+package com.kacper.wedding_planner.controller;
+
+import com.kacper.wedding_planner.config.CustomUserDetails;
+import com.kacper.wedding_planner.model.User;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import java.io.IOException;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@WebMvcTest(WeddingGamesController.class)
+@ExtendWith(SpringExtension.class)
+public class WeddingGamesControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private WeddingGamesController weddingGamesController;
+
+    @BeforeEach
+    void setupSecurityContext() {
+
+        User user = new User();
+        user.setEmail("test@example.com");
+
+        CustomUserDetails principal = new CustomUserDetails(user);
+
+        UsernamePasswordAuthenticationToken authentication =
+                new UsernamePasswordAuthenticationToken(principal, null, principal.getAuthorities());
+
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+    }
+
+
+    @WithMockUser
+    @Test
+    public void shouldWeddingGamesPageReturnsViewWithQuestions() throws Exception {
+
+        weddingGamesController.init();
+        // Act & Assert
+        mockMvc.perform(get("/wedding_games")
+                        .accept(MediaType.TEXT_HTML))
+                .andExpect(status().isOk())
+                .andExpect(view().name("wedding_games"))
+                .andExpect(model().attributeExists("questions"));
+    }
+
+
+}
