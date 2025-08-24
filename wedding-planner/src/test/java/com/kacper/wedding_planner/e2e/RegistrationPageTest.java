40b16438f45169d562ab01a4e2f8e2b4ca9ed6a3
Kacper Szabat <kacper.szabat99@gmail.com>
2025-08-24 21:08:13 +0200

    Writing a e2e test testRegistrationFormElementsAreVisible

---
+++ wedding-planner/src/test/java/com/kacper/wedding_planner/e2e/RegistrationPageTest.java
@@ -0,0 +1,45 @@
+package com.kacper.wedding_planner.e2e;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.springframework.security.core.parameters.P;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class RegistrationPageTest {
+
+    private WebDriver driver;
+
+    @BeforeEach
+    public void setUp() {
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        driver.get("http://localhost:8080/register");
+    }
+
+    @AfterEach
+    public void tearDown() {
+        if (driver != null) {
+            driver.quit();
+        }
+    }
+
+    @Test
+    public void testRegistrationFormElementsAreVisible() {
+        WebElement nameField = driver.findElement(By.id("firstName"));
+        WebElement emailField = driver.findElement(By.id("email"));
+        WebElement passwordField = driver.findElement(By.id("password"));
+        WebElement registerButton = driver.findElement(By.cssSelector("button[type='submit']"));
+
+        assertTrue(nameField.isDisplayed());
+        assertTrue(emailField.isDisplayed());
+        assertTrue(passwordField.isDisplayed());
+        assertTrue(registerButton.isDisplayed());
+    }
+}
