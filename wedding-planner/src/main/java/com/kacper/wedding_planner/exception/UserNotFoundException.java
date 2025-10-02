53e0629377d432ba3aaee711819c7763140cb69c
Kacper Szabat <kacper.szabat99@gmail.com>
2025-10-02 19:24:09 +0200

    Creating a new exception UserNotFoundException

---
+++ wedding-planner/src/main/java/com/kacper/wedding_planner/exception/UserNotFoundException.java
@@ -0,0 +1,8 @@
+package com.kacper.wedding_planner.exception;
+
+public class UserNotFoundException extends RuntimeException {
+
+    public UserNotFoundException(String email) {
+        super("User with email " + email + " not found.");
+    }
+}
