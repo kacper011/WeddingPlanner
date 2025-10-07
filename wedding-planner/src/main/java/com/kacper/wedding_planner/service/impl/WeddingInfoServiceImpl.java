a157e286898c2986235a228d765c56f881fa9082
Kacper Szabat <kacper.szabat99@gmail.com>
2025-10-07 21:09:38 +0200

    Add a Transactional and change Autowired to constructor

--- wedding-planner/src/main/java/com/kacper/wedding_planner/service/impl/WeddingInfoServiceImpl.java
+++ wedding-planner/src/main/java/com/kacper/wedding_planner/service/impl/WeddingInfoServiceImpl.java
@@ -4,15 +4,20 @@ import com.kacper.wedding_planner.model.User;
 import com.kacper.wedding_planner.model.WeddingInfo;
 import com.kacper.wedding_planner.repository.WeddingInfoRepository;
 import com.kacper.wedding_planner.service.WeddingInfoService;
-import org.springframework.beans.factory.annotation.Autowired;
+import jakarta.transaction.Transactional;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
 @Service
 public class WeddingInfoServiceImpl implements WeddingInfoService {
 
-    @Autowired
-    private WeddingInfoRepository weddingInfoRepository;
+    private final WeddingInfoRepository weddingInfoRepository;
+
+    public WeddingInfoServiceImpl(WeddingInfoRepository weddingInfoRepository) {
+        this.weddingInfoRepository = weddingInfoRepository;
+    }
+
+    @Transactional
     @Override
     public void saveOrUpdateWeddingInfo(WeddingInfo weddingInfo, User user) {
         Optional<WeddingInfo> existing = weddingInfoRepository.findByUser(user);
