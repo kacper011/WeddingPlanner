f2fd776bf0f2ede185b63205c8d5343c10c107b8
Kacper Szabat <kacper.szabat99@gmail.com>
2025-10-18 19:53:53 +0200

    Changing field names from Polish to English in template expenses.html

--- wedding-planner/src/main/java/com/kacper/wedding_planner/model/GuestTable.java
+++ wedding-planner/src/main/java/com/kacper/wedding_planner/model/GuestTable.java
@@ -26,7 +26,7 @@ public class GuestTable {
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "user_id")
     private User user;
-    @OneToMany(mappedBy = "tables")
+    @OneToMany(mappedBy = "table")
     private List<Guest> guests;
 
 }
