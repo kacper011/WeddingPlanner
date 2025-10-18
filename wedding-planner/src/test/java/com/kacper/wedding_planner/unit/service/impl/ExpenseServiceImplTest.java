771822d79607a9074497e9b70a27e8d59e04b42e
Kacper Szabat <kacper.szabat99@gmail.com>
2025-10-18 19:42:07 +0200

    Changing field names from Polish to English in ExpenseServiceImplTest

--- wedding-planner/src/test/java/com/kacper/wedding_planner/unit/service/impl/ExpenseServiceImplTest.java
+++ wedding-planner/src/test/java/com/kacper/wedding_planner/unit/service/impl/ExpenseServiceImplTest.java
@@ -40,8 +40,8 @@ class ExpenseServiceImplTest {
         User user = new User();
         user.setEmail("test@example.com");
         Expense expense = new Expense();
-        expense.setNazwa("Test");
-        expense.setKwota(BigDecimal.valueOf(100));
+        expense.setName("Test");
+        expense.setAmount(BigDecimal.valueOf(100));
         expense.setUser(user);
 
         when(userRepository.findByEmail("test@example.com")).thenReturn(Optional.of(user));
@@ -50,7 +50,7 @@ class ExpenseServiceImplTest {
         var result = expenseService.getExpensesForUser("test@example.com");
 
         assertEquals(1, result.size());
-        assertEquals("Test", result.get(0).getNazwa());
+        assertEquals("Test", result.get(0).getName());
     }
 
     @Test
@@ -67,12 +67,12 @@ class ExpenseServiceImplTest {
         user.setEmail("test@example.com");
 
         Expense e1 = new Expense();
-        e1.setKwota(BigDecimal.valueOf(100));
-        e1.setNazwa("Test1");
+        e1.setAmount(BigDecimal.valueOf(100));
+        e1.setName("Test1");
 
         Expense e2 = new Expense();
-        e2.setKwota(BigDecimal.valueOf(200));
-        e2.setNazwa("Test2");
+        e2.setAmount(BigDecimal.valueOf(200));
+        e2.setName("Test2");
 
         when(userRepository.findByEmail("test@example.com")).thenReturn(Optional.of(user));
         when(expenseRepository.findByUser(user)).thenReturn(Arrays.asList(e1, e2));
@@ -88,8 +88,8 @@ class ExpenseServiceImplTest {
         user.setEmail("test@example.com");
 
         Expense e1 = new Expense();
-        e1.setKwota(null);
-        e1.setNazwa("Test1");
+        e1.setAmount(null);
+        e1.setName("Test1");
 
         when(userRepository.findByEmail("test@example.com")).thenReturn(Optional.of(user));
         when(expenseRepository.findByUser(user)).thenReturn(Collections.singletonList(e1));
@@ -104,8 +104,8 @@ class ExpenseServiceImplTest {
         user.setEmail("test@example.com");
 
         Expense expense = new Expense();
-        expense.setNazwa("Test");
-        expense.setKwota(BigDecimal.valueOf(50));
+        expense.setName("Test");
+        expense.setAmount(BigDecimal.valueOf(50));
 
         expenseService.saveExpense(expense, user);
 
@@ -113,8 +113,8 @@ class ExpenseServiceImplTest {
         verify(expenseRepository).save(captor.capture());
 
         Expense savedExpense = captor.getValue();
-        assertEquals("Test", savedExpense.getNazwa());
-        assertEquals(BigDecimal.valueOf(50), savedExpense.getKwota());
+        assertEquals("Test", savedExpense.getName());
+        assertEquals(BigDecimal.valueOf(50), savedExpense.getAmount());
         assertEquals(user, savedExpense.getUser());
     }
 
