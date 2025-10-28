78b6973146b02fe4fbe2e9ecf4781f1385f98785
Kacper Szabat <kacper.szabat99@gmail.com>
2025-10-28 20:59:16 +0100

    Change of test testSuccessfulLogin

--- ui-tests/src/test/java/tests/LoginTest.java
+++ ui-tests/src/test/java/tests/LoginTest.java
@@ -2,8 +2,13 @@ package tests;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import pages.LoginPage;
 import utils.BaseTest;
+
+import java.time.Duration;
 
 public class LoginTest extends BaseTest {
 
@@ -22,9 +27,12 @@ public class LoginTest extends BaseTest {
         loginPage.enterPassword("test123");
         loginPage.clickLogin();
 
-        String expectedUrl = "http://localhost:8080/guests";
-        String actualUrl = driver.getCurrentUrl();
-        Assertions.assertEquals(expectedUrl, actualUrl, "After logging in, I was not redirected to /guests.");
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(5));
+        wait.until(ExpectedConditions.urlToBe("http://localhost:8080/guests"));
+
+        By header = By.cssSelector("h1.text-center.mb-4");
+        wait.until(ExpectedConditions.visibilityOfElementLocated(header));
+        Assertions.assertTrue(driver.findElement(header).isDisplayed(), "The “Wedding Guest List” heading is not visible after logging in.");
 
     }
 }
