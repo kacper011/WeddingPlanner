7e42e51203414c273917429f195c84430ea847ea
Kacper Szabat <kacper.szabat99@gmail.com>
2025-11-03 21:09:27 +0100

    Writing a testGuestsPageElementsVisible

---
+++ ui-tests/src/test/java/tests/GuestsPageTest.java
@@ -0,0 +1,31 @@
+package tests;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import pages.GuestsPage;
+import pages.LoginPage;
+import utils.BaseTest;
+
+import java.time.Duration;
+
+public class GuestsPageTest extends BaseTest {
+
+    @Test
+    public void testGuestsPageElementsVisible() {
+        LoginPage loginPage = new LoginPage(driver);
+        loginPage.enterEmail("test@gmail.com");
+        loginPage.enterPassword("test1234");
+        loginPage.clickLogin();
+
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        wait.until(ExpectedConditions.urlContains("/guests"));
+
+        GuestsPage guestsPage = new GuestsPage(driver);
+
+        Assertions.assertTrue(guestsPage.isNavbarVisible(), "Navbar is not visible!");
+        Assertions.assertTrue(guestsPage.isGuestsTableVisible(), "Guest table is not visible!");
+        Assertions.assertTrue(guestsPage.isStatsVisible(), "Stats section not visible!");
+    }
+}
