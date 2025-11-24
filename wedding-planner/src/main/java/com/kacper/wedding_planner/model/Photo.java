c3bf2a999b51dfa90ab1a67394fc169f00c9e0bd
Kacper Szabat <kacper.szabat99@gmail.com>
2025-11-24 20:26:29 +0100

    Adding a Photo entity

---
+++ wedding-planner/src/main/java/com/kacper/wedding_planner/model/Photo.java
@@ -0,0 +1,33 @@
+package com.kacper.wedding_planner.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "photos")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Photo {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(optional = false)
+    @JoinColumn(name = "owner_id")
+    private User owner;
+
+    private String filename;
+    private String storagePath;
+    private String contentType;
+    private Long size;
+    private boolean uploadedByPublic;
+    private LocalDateTime uploadedAt = LocalDateTime.now();
+}
