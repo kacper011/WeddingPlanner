1fd07ac289391081fc684c249c25df0fbdeb5f88
Kacper Szabat <kacper.szabat99@gmail.com>
2025-11-24 21:21:02 +0100

    Adding a UploadToken entity

---
+++ wedding-planner/src/main/java/com/kacper/wedding_planner/model/UploadToken.java
@@ -0,0 +1,34 @@
+package com.kacper.wedding_planner.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "upload_tokens")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class UploadToken {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true, nullable = false)
+    private String token;
+
+    @ManyToOne(optional = false)
+    @JoinColumn(name = "owner_id")
+    private User owner;
+
+    private LocalDateTime createdAt = LocalDateTime.now();
+    private LocalDateTime expiresAt;
+    private Integer maxUses;
+    private Integer uses = 0;
+}
