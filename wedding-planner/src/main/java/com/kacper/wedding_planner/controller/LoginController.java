235033b528a9aaa44b2f0ccd5c2df4b8c577ad67
Kacper Szabat <kacper.szabat99@gmail.com>
2025-12-02 20:12:07 +0100

    Method loginPage is more secure right now

--- wedding-planner/src/main/java/com/kacper/wedding_planner/controller/LoginController.java
+++ wedding-planner/src/main/java/com/kacper/wedding_planner/controller/LoginController.java
@@ -1,13 +1,23 @@
 package com.kacper.wedding_planner.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class LoginController {
 
     @GetMapping("/login")
-    public String loginPage() {
+    public String loginPage(@RequestParam(required = false) String error,
+                            @RequestParam(required = false) String logout,
+                            Model model) {
+        if (error != null) {
+            model.addAttribute("errorMessage", "Nieprawidłowy login lub hasło");
+        }
+        if (logout != null) {
+            model.addAttribute("msg", "Zostałeś wylogowany pomyślnie");
+        }
         return "login";
     }
 }
