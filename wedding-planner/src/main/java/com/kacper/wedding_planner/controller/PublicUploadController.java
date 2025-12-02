128b64b91fc18fc42f4871ed09e314d1059fa7fc
Kacper Szabat <kacper.szabat99@gmail.com>
2025-12-02 20:27:20 +0100

    Method upload is more secure right now

--- wedding-planner/src/main/java/com/kacper/wedding_planner/controller/PublicUploadController.java
+++ wedding-planner/src/main/java/com/kacper/wedding_planner/controller/PublicUploadController.java
@@ -3,6 +3,8 @@ package com.kacper.wedding_planner.controller;
 import com.kacper.wedding_planner.model.UploadToken;
 import com.kacper.wedding_planner.repository.UploadTokenRepository;
 import com.kacper.wedding_planner.service.PhotoService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
@@ -16,6 +18,8 @@ public class PublicUploadController {
 
     private final UploadTokenRepository tokenRepository;
     private final PhotoService photoService;
+    private static final Logger log = LoggerFactory.getLogger(PublicUploadController.class);
+
 
     public PublicUploadController(UploadTokenRepository tokenRepository, PhotoService photoService) {
         this.tokenRepository = tokenRepository;
@@ -35,25 +39,32 @@ public class PublicUploadController {
     }
 
     @PostMapping("/upload/{token}")
-    public String upload(
-            @PathVariable String token,
-            @RequestParam("file") MultipartFile file,
-            Model model
-    ) {
+    public String upload(@PathVariable String token,
+                         @RequestParam("file") MultipartFile file,
+                         Model model) {
+
         Optional<UploadToken> ot = tokenRepository.findByToken(token);
 
         if (ot.isEmpty() || !ot.get().isValid()) {
+            log.warn("Nieprawidłowy token publiczny: {}", token);
             return "public_invalid_token";
+        }
+
+        if (file.isEmpty() || !file.getContentType().startsWith("image/") || file.getSize() > 5*1024*1024) {
+            model.addAttribute("error", "Nieprawidłowy plik");
+            model.addAttribute("token", token);
+            return "public_upload_form";
         }
 
         try {
             photoService.savePhotoFromUpload(file, ot.get());
             return "public_upload_success";
-
         } catch (Exception ex) {
-            model.addAttribute("error", ex.getMessage());
+            log.error("Błąd zapisu pliku: {}", ex.getMessage(), ex);
+            model.addAttribute("error", "Nie udało się zapisać pliku");
             model.addAttribute("token", token);
             return "public_upload_form";
         }
     }
+
 }
