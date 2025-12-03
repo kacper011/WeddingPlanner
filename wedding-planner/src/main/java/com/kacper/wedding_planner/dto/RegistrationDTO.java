9a983d8ebeccf61761acf8e35bec9ad0ccb3a8ae
Kacper Szabat <kacper.szabat99@gmail.com>
2025-12-03 21:54:14 +0100

    RegistrationDTO is more secure right now

--- wedding-planner/src/main/java/com/kacper/wedding_planner/dto/RegistrationDTO.java
+++ wedding-planner/src/main/java/com/kacper/wedding_planner/dto/RegistrationDTO.java
@@ -1,13 +1,26 @@
 package com.kacper.wedding_planner.dto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
 public class RegistrationDTO {
+
+    @NotBlank(message = "Email jest wymagany")
+    @Email(message = "Nieprawidłowy format email")
     private String email;
+
+    @NotBlank(message = "Imię jest wymagane")
     private String firstName;
+
+    @NotBlank(message = "Hasło jest wymagane")
+    @Size(min = 8, message = "Hasło musi mieć minimum 8 znaków")
     private String password;
+
+    @NotBlank(message = "Potwierdzenie hasła jest wymagane")
     private String confirmPassword;
 }
