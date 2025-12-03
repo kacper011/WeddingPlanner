526fca72b744787b0c72bafe4b6f91448c18faf5
Kacper Szabat <kacper.szabat99@gmail.com>
2025-12-03 21:57:06 +0100

    Method registerUser is more secure right now

--- wedding-planner/src/main/java/com/kacper/wedding_planner/controller/RegistrationController.java
+++ wedding-planner/src/main/java/com/kacper/wedding_planner/controller/RegistrationController.java
@@ -3,9 +3,11 @@ package com.kacper.wedding_planner.controller;
 import com.kacper.wedding_planner.dto.RegistrationDTO;
 import com.kacper.wedding_planner.dto.UserDTO;
 import com.kacper.wedding_planner.service.UserService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -23,10 +25,15 @@ public class RegistrationController {
     }
 
     @PostMapping("/register")
-    public String registerUser(@ModelAttribute("registrationDto") RegistrationDTO dto, Model model) {
+    public String registerUser(@ModelAttribute("registrationDto") @Valid RegistrationDTO dto,
+                               BindingResult bindingResult, Model model) {
+
+        if (bindingResult.hasErrors()) {
+            return "register";
+        }
 
         if (!dto.getPassword().equals(dto.getConfirmPassword())) {
-            model.addAttribute("error", "The passwords are not the same!");
+            model.addAttribute("error", "Hasła nie są takie same!");
             return "register";
         }
 
