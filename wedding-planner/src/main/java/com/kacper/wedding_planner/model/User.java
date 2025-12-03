6ad68c0914097f00b87a6598f8cf7dfbc778b2c5
Kacper Szabat <kacper.szabat99@gmail.com>
2025-12-03 21:52:37 +0100

    Entity User is more secure right now

--- wedding-planner/src/main/java/com/kacper/wedding_planner/model/User.java
+++ wedding-planner/src/main/java/com/kacper/wedding_planner/model/User.java
@@ -1,7 +1,10 @@
 package com.kacper.wedding_planner.model;
 
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
 import lombok.*;
 
 import java.util.ArrayList;
@@ -18,15 +21,20 @@ public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @NotBlank
+    @Email
     @Column(unique = true, nullable = false)
     private String email;
+    @JsonIgnore
     @Column(nullable = false)
     private String password;
+    @NotBlank
     @Column(name = "first_name", nullable = false)
     private String firstName;
-    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true, fetch = FetchType.LAZY)
     private List<Guest> guests = new ArrayList<>();
-    @OneToOne(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
+
+    @OneToOne(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true, fetch = FetchType.LAZY)
     private WeddingInfo weddingInfo;
 
 }
