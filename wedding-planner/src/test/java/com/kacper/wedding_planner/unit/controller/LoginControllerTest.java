e5a1a864d9be16d948ada31bbd224439d4cddfe5
Kacper Szabat <kacper.szabat99@gmail.com>
2026-01-03 19:41:52 +0100

    Fix login template parsing in tests by handling CSRF

--- wedding-planner/src/test/java/com/kacper/wedding_planner/unit/controller/LoginControllerTest.java
+++ wedding-planner/src/test/java/com/kacper/wedding_planner/unit/controller/LoginControllerTest.java
@@ -7,9 +7,9 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.web.servlet.MockMvc;
 
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @WebMvcTest(LoginController.class)
 @AutoConfigureMockMvc(addFilters = false)
@@ -19,9 +19,32 @@ class LoginControllerTest {
     private MockMvc mockMvc;
 
     @Test
-    void shouldReturnLoginView() throws Exception {
-        mockMvc.perform(get("/login"))
+    void shouldReturnLoginViewWithoutParams() throws Exception {
+        mockMvc.perform(get("/login")
+                        .with(csrf()))
                 .andExpect(status().isOk())
-                .andExpect(view().name("login"));
+                .andExpect(view().name("login"))
+                .andExpect(model().attributeDoesNotExist("errorMessage"))
+                .andExpect(model().attributeDoesNotExist("msg"));
+    }
+
+    @Test
+    void shouldShowErrorMessageWhenErrorParamPresent() throws Exception {
+        mockMvc.perform(get("/login").param("error", "true")
+                        .with(csrf()))
+                .andExpect(status().isOk())
+                .andExpect(view().name("login"))
+                .andExpect(model().attributeExists("errorMessage"))
+                .andExpect(model().attribute("errorMessage", "Nieprawidłowy login lub hasło"));
+    }
+
+    @Test
+    void shouldShowLogoutMessageWhenLogoutParamPresent() throws Exception {
+        mockMvc.perform(get("/login").param("logout", "true")
+                        .with(csrf()))
+                .andExpect(status().isOk())
+                .andExpect(view().name("login"))
+                .andExpect(model().attributeExists("msg"))
+                .andExpect(model().attribute("msg", "Zostałeś wylogowany pomyślnie"));
     }
 }
